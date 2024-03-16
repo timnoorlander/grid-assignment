c0dc0923f28abf7980413df73bc81cc97b6be0f2
Tim Noorlander <tgnoorlander@gmail.com>
2024-03-16 19:27:19 +0100

    mock api in production env

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,6 @@ import { GlobalStyles } from "./styles/global-styles.ts";
 import { AppRoutes } from "./routes/index.tsx";
 
 async function enableMocking() {
-  if (process.env.NODE_ENV !== "development") {
-    return;
-  }
-
   const { worker } = await import("./mocks/browser");
   return worker.start();
 }
